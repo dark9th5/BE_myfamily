9d27f779a36f137f5ddc91697feab4ce6607409b
dark9th5 <giolanhluc@gmail.com>
2025-10-12 15:25:52 +0700

    ok

--- src/main/kotlin/com/lc9th5/myfamily/model/user/User.kt
+++ src/main/kotlin/com/lc9th5/myfamily/model/user/User.kt
@@ -1,10 +1,16 @@
 package com.lc9th5.myfamily.model.user
 
+import jakarta.persistence.CollectionTable
 import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
 import java.time.LocalDateTime
@@ -26,6 +32,12 @@ data class User(
 
     @Column(name = "full_name")
     val fullName: String? = null,
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = [JoinColumn(name = "user_id")])
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    var roles: MutableSet<Role> = mutableSetOf(Role.USER),
 
     @Column(name = "created_at")
     @CreationTimestamp
